f172a9d28b828b55bb421d44201e5aaa4b414f08
Zolars <zolars@outlook.com>
2019-06-29 01:53:20 +0800

    2019-06-29 01:53 Stable version for weibo spider

--- weibospider.py
+++ weibospider.py
@@ -1,11 +1,14 @@
 # coding: utf-8
 import sys
+import gc
 import re
 import json
 import time
 import emoji
 import requests
+import random
 import pymysql
+import chartify
 import datetime
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
@@ -13,10 +16,47 @@ from concurrent.futures import ThreadPoolExecutor
 
 class Weibospider():
     def __init__(self, query_val):
-        self.url_template = "https://m.weibo.cn/api/container/getIndex?type=wb&queryVal={}&containerid=100103type=2%26q%3D{}&page={}"
-        self.query_val = query_val
-        self.log = open('weibospider{:_%m_%d_%H_%M}.log'.format(
+
+        # link and user-agent setting
+        # ?type=wb&queryVal={}&containerid=100103type=2%26q%3D{}&page={}
+        self._url_template = "https://m.weibo.cn/api/container/getIndex?type=wb&queryVal={}&containerid=100103type=2%26q%3D{}&page={}"
+        self._query_val = query_val
+
+        self.user_agents = ['Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+                            'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36',
+                            'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; rv:11.0) like Gecko)',
+                            'Mozilla/5.0 (Windows; U; Windows NT 5.2) Gecko/2008070208 Firefox/3.0.1',
+                            'Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070309 Firefox/2.0.0.3',
+                            'Mozilla/5.0 (Windows; U; Windows NT 5.1) Gecko/20070803 Firefox/1.5.0.12',
+                            'Opera/9.27 (Windows NT 5.2; U; zh-cn)',
+                            'Mozilla/5.0 (Macintosh; PPC Mac OS X; U; en) Opera 8.0',
+                            'Opera/8.0 (Macintosh; PPC Mac OS X; U; en)',
+                            'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.12) Gecko/20080219 Firefox/2.0.0.12 Navigator/9.0.0.6',
+                            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Win64; x64; Trident/4.0)',
+                            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)',
+                            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.2; .NET4.0C; .NET4.0E)',
+                            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Maxthon/4.0.6.2000 Chrome/26.0.1410.43 Safari/537.1 ',
+                            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.2; .NET4.0C; .NET4.0E; QQBrowser/7.3.9825.400)',
+                            'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0 ',
+                            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.92 Safari/537.1 LBBROWSER',
+                            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0; BIDUBrowser 2.x)',
+                            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/3.0 Safari/536.11']
+
+        # Connect to MySQL
+        self._db = pymysql.connect(
+            host='localhost',  # mysql server address
+            port=3306,  # port num
+            user='root',  # username
+            passwd='root',  # password
+            db='weibo',
+            charset='utf8',
+        )
+        self._cur = self._db.cursor()
+        self._log = open('./log/weibospider{:_%m_%d_%H_%M}.log'.format(
             datetime.datetime.now()), 'a', encoding='utf-8')
+
+    def __del__(self):
+        self._db.close()
 
     def clean_text(self, text):
         """clean tags or emoji of texts"""
@@ -30,17 +70,22 @@ class Weibospider():
         dd = emoji.demojize(dd)
         return dd.strip()
 
-    def catch_data(self, query_val, page_id, cur):
+    def catch_data(self, page_id):
         """catch one page's data"""
-        resp = requests.get(self.url_template.format(
-            query_val, query_val, page_id))
-
-        # print(self.url_template.format(query_val, query_val, page_id))
+        headers = {'User-Agent': self.user_agents[random.randrange(
+            0, len(self.user_agents))]}
+        resp = requests.get(
+            url=self._url_template.format(
+                self._query_val,
+                self._query_val,
+                page_id),
+            headers=headers)
 
         card_group = json.loads(resp.text)['data']['cards'][0]['card_group']
 
-        effect_row = 0
+        effect_rows = 0
         for card in card_group:
+
             mblog = card['mblog']
 
             time = mblog['created_at']
@@ -61,79 +106,99 @@ class Weibospider():
                     sendtime = datetime.datetime.strptime(time, '%Y-%m-%d')
 
             time = "{:%Y-%m-%d %H:%M}".format(sendtime - delta)
+            result = [
+                mblog['id'],  # weibo id
+                self.clean_text(mblog['text']).strip(
+                    '\n').encode("utf8"),  # text
+                time,  # time of posts
+                str(mblog['user']['id']),  # userid
+                mblog['user']['screen_name'],  # username
+                mblog['reposts_count'],  # reposts num
+                mblog['comments_count'],  # comments num
+                mblog['attitudes_count'],  # attitudes num
+            ]
 
             # save to mysql
-            sql_insert = (
-                'insert ignore into result (mid, text, time, userid, username, reposts_count, comments_count, attitudes_count) values ({},"{}","{}","{}","{}",{},{},{}); '.format(
-                    mblog['id'],  # weibo id
-                    self.clean_text(mblog['text']),  # text
-                    time,  # time of posts
-                    str(mblog['user']['id']),  # userid
-                    mblog['user']['screen_name'],  # username
-                    mblog['reposts_count'],  # reposts num
-                    mblog['comments_count'],  # comments num
-                    mblog['attitudes_count'],  # attitudes num
-                )).strip('\n')
-            sql_insert = sql_insert.encode("utf8")
-            effect_row += cur.execute(sql_insert)
+            try:
+                sql = 'insert ignore into result (mid, text, time, userid, username, reposts_count, comments_count, attitudes_count) values (%s,%s,%s,%s,%s,%s,%s,%s);'
+                effect_rows += self._cur.execute(sql, result)
+                self._db.commit()
+            except Exception as e:
+                print("MySQL ERROR: \t", e)
+        try:
+            self._log.write(
+                "{:%Y-%m-%d %H:%M:%S} Success: Update {} date at page {}.\tURL: {}\n".format(
+                    datetime.datetime.now(),
+                    effect_rows,
+                    page_id,
+                    self._url_template.format(
+                        self._query_val,
+                        self._query_val,
+                        page_id)))
+        except Exception as e:
+            print("Log ERROR: \t", e)
 
-        self.log.write(
-            "{:%Y-%m-%d %H:%M:%S} Success: Update {} date at page {}.\tURL: {}\n".format(
-                datetime.datetime.now(),
-                effect_row,
-                page_id,
-                self.url_template.format(
-                    self.query_val, self.query_val, page_id)
-            ))
-
-        return effect_row
+        return effect_rows
 
     def catch_pages(self, start_page_num, end_page_num, delay):
         """Catch data by keywords and pages"""
 
-        # 连接数据库
-        db = pymysql.connect(
-            host='localhost',  # mysql server address
-            port=3306,  # port num
-            user='root',  # username
-            passwd='root',  # password
-            db='weibo',
-            charset='utf8',
-        )
-        cur = db.cursor()
-        effect_row = 0
-        for page_id in range(start_page_num, end_page_num):
-            try:
-                effect_row += self.catch_data(self.query_val, page_id, cur)
-                time.sleep(delay)
-            except:
-                self.log.write(
-                    "{:%Y-%m-%d %H:%M:%S} Error: Data not found at page {}.\tURL: {}\n".format(
-                        datetime.datetime.now(),
-                        page_id,
-                        self.url_template.format(
-                            self.query_val, self.query_val, page_id)
-                    ))
-                time.sleep(delay)
-
-        db.commit()
-        db.close()
+        page_id = start_page_num
+        effect_rows = 0
+        error_list = []
+        with tqdm(total=end_page_num - start_page_num + 1) as pbar:
+            while page_id <= end_page_num:
+                try:
+                    effect_rows += self.catch_data(page_id)
+                    page_id += 1
+                    pbar.update(1)
+                    time.sleep(delay)
+                except:
+                    if page_id not in error_list:
+                        error_list.append(page_id)
+                        time.sleep(delay)
+                    else:
+                        try:
+                            self._log.write(
+                                "{:%Y-%m-%d %H:%M:%S} Error: Data not found at page {}.\tURL: {}\n".format(
+                                    datetime.datetime.now(),
+                                    page_id,
+                                    self._url_template.format(
+                                        self._query_val,
+                                        self._query_val,
+                                        page_id)))
+                        except Exception as e:
+                            print("Log ERROR: \t", e)
+                        page_id += 1
+                        pbar.update(1)
 
 
 def demo(target):
     print('Spider beginning...')
-    weibospiderBig = Weibospider(target)
+
     executor = ThreadPoolExecutor(max_workers=8)
 
     while True:
-        print('Epoch begin...')
-        for i in range(210, 0, -30):
-            executor.submit(weibospiderBig.catch_pages, i, i + 15, 0)
-            executor.submit(weibospiderBig.catch_pages, i + 15, i + 30, 0)
+        for i in range(180, 0, -20):
+
+            spider = Weibospider(target)
+            executor.submit(spider.catch_pages, i, i + 5, 0)
+            del spider
+            time.sleep(3)
+
+            spider = Weibospider(target)
+            executor.submit(spider.catch_pages, i + 5, i + 10, 0)
+            del spider
             time.sleep(10)
 
 
+def stable(target):
+    print('Spider beginning...')
+    weibospiderBig = Weibospider(target)
+    while True:
+        weibospiderBig.catch_pages(0, 5, 0)
+
+
 if __name__ == '__main__':
-    target = '吴亦凡'
+    target = '李鑫一'
     demo(target)
-    # stable(target)
